177c057192d7439d9ad4f003de34719d7266e194
Calista Ifenkwe <99617187+TechyStarr@users.noreply.github.com>
2024-08-15 16:08:16 +0100

    recent updates

--- src/components/contact/ContactHeaderSection.vue
+++ src/components/contact/ContactHeaderSection.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="relative lg:mt-header-margin">
-      <img v-lazy="require('@/assets/header-img.png')" alt="Header Image" class="w-full h-[449px] object-cover">
+      <img v-lazy="require('@/assets/Frame12.png')" alt="Header Image" class="w-full h-[449px] object-cover">
       <div ref="contactText" class="absolute top-64 md:top-40 left-4 md:left-6 px-6 md:px-20 py-16 md:py-32 space-y-4 text-white">
         <h1 class="text-[32px] md:text-[64px] w-full md:w-[720px] text-center md:text-left font-extrabold leading-tight">
           Contact
